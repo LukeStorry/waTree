8cd1a3911fbb9fa71f7eaab38943fac736c083f1
Luke Storry <luke@lukestorry.co.uk>
2018-11-07 21:04:16 +0000

    cleaned up code and added auto-decay

--- forest.js
+++ forest.js
@@ -6,7 +6,7 @@ function setup() {
   forest = loadImage("assets/forest.png");
   tree = loadImage("assets/tree.png");
 
-  starting_health = displayHeight*0.7;
+  starting_health = displayHeight * 0.7;
   health1 = starting_health;
   health2 = starting_health;
   health3 = starting_health;
@@ -17,22 +17,26 @@ function draw() {
   clear();
 
   tint(230, 255, 240, 150);
-  image(forest, 0, 0, width, height)
+  image(forest, 0, 0, width, height);
 
   tint(200, 50, 50, calculate_transparency(health1));
-  image(tree, width/4 - health1 / 4, height - health1, health1 / 3, health1);
+  image(tree, width / 4 - health1 / 4, height - health1, health1 / 3, health1);
 
   tint(0, 200, 0, calculate_transparency(health2));
-  image(tree, width/2 - health2 / 4, height - health2, health2 / 3, health2);
+  image(tree, width / 2 - health2 / 4, height - health2, health2 / 3, health2);
 
   tint(0, 0, 200, calculate_transparency(health3));
-  image(tree, 3*width/4 - health3 / 4, height - health3, health3 / 3, health3);
+  image(tree, 3 * width / 4 - health3 / 4, height - health3, health3 / 3, health3);
+
+  health1 -= 0.1;
+  health2 -= 0.1;
+  health3 -= 0.1;
 }
 
 function mousePressed() {
-  if (mouseX < width/3) {
+  if (mouseX < width / 3) {
     health1 = update(health1);
-  } else if (mouseX < 2*width/3) {
+  } else if (mouseX < 2 * width / 3) {
     health2 = update(health2);
   } else {
     health3 = update(health3);
@@ -40,13 +44,13 @@ function mousePressed() {
 }
 
 function calculate_transparency(health) {
-  return 150 * health / starting_health +50
+  return 150 * health / starting_health + 50;
 }
 
 function update(health) {
   if (mouseY < height / 2 && health < starting_health * 1.5) {
     return health + 20;
-  } else if (health > starting_health * 0.7 ) {
+  } else if (health > starting_health * 0.7) {
     return health - 20;
   } else {
     return health;
