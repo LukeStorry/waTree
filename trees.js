c49e34b6305dd494cc737494db138768688bcd57
Luke Storry <luke@lukestorry.co.uk>
2018-11-20 23:41:28 +0000

    fixed window sizing

--- trees.js
+++ trees.js
@@ -4,41 +4,33 @@ var treeData = [{name:"Aleena", size:5,  species:"Small",  item:[]},
                 {name:"Luke",   size:15, species:"Small",  item:[]},
                 {name:"Ben",    size:10, species:"Big",    item:[]}]; // TODO - replace with pulling in data
 
-var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    x = w.innerWidth || e.clientWidth || g.clientWidth,
-    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+var graphWidth = window.innerWidth;
+var graphHeight = window.innerHeight;
 
 var treeGraph = d3.select('svg')
-   .attr("width", x)
-   .attr("height", y);
+  .attr("width", graphWidth)
+  .attr("height", graphHeight);
 
+var biggestTreeSize = treeData.reduce(function(biggest, next) {
+  return biggest.size > next.size ? biggest : next;
+}).size;
+
+// TODO normalise the incoming data instead of growing trees according to largest
+var scaleFactor = graphHeight / (biggestTreeSize * 1.2);
 var barPadding = 5;
-var graphHeight = y;
-var treeWidth = x/(treeData.length);
-
-var biggestTreeSize = 0;
-for (var i = 0; i < treeData.length; i++) {
-  if (treeData[i].size > biggestTreeSize)
-    biggestTreeSize = treeData[i].size;
-}
-
-var scaleFactor = graphHeight/(biggestTreeSize*1.2);
+var treeWidth = graphWidth / (treeData.length);
 
 var barChart = treeGraph.selectAll("rect")
   .data(treeData)
   .enter()
   .append("rect")
   .attr("y", function(d) {
-      return (graphHeight - (d.size*scaleFactor));
+    return (graphHeight - (d.size * scaleFactor));
   })
-  .attr("height", function(d) {
-      return (d.size * scaleFactor);
+  .attr("height", function(tree) {
+    return (tree.size * scaleFactor);
   })
   .attr("width", treeWidth - barPadding)
-  .attr("transform", function (d, i) {
-      var translate = [treeWidth * i, 0];
-      return "translate("+ translate +")";
+  .attr("transform", function(tree, i) {
+    return "translate(" + [treeWidth * i + barPadding / 2, 0] + ")";
   });
