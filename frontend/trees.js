a8d8c033ac1b02c999e066edf86d68bdfb7ad08b
AleenaCodes <AleenaCodes@users.noreply.github.com>
2018-12-03 10:22:01 +0000

    Revert "Added to frontend - names and rainclouds, now setup to receive data from API once data is available"
    This reverts commit 935eafb3fad486feac3a8cd1ba33f29297a6b5fc.

--- frontend/trees.js
+++ frontend/trees.js
@@ -1,90 +1,65 @@
 d3.json("http://localhost:3030/all")
-.then(function(treeData){
+.then(function(data){
+  console.log(data);
+});
 
-  var treeData = [{"User": 1, "UserName": "user1", "Score": 50, "isRaining": true},
-                  {"User": 2, "UserName": "user2", "Score": 60, "isRaining": false},
-                  {"User": 3, "UserName": "user3", "Score": 40, "isRaining": true},
-                  {"User": 4, "UserName": "user4", "Score": 100, "isRaining": false},
-                 ];
-  // TODO - take out once DB has proper data
+var treeData = [{name:"Aleena", size:5,  species:"Small",  item:[]},
+                {name:"Rae",    size:12, species:"Big",    item:["glasses", "hat"]},
+                {name:"Sunny",  size:8,  species:"Medium", item:["glasses"]},
+                {name:"Luke",   size:15, species:"Small",  item:[]},
+                {name:"Ben",    size:10, species:"Big",    item:["shoes", "hat"]}]; // TODO - replace with pulling in data
 
-  var barPadding = 5;
-  var graphHeight = document.getElementById('treesRow').offsetHeight;
-  var graphWidth = document.getElementById('treesRow').scrollWidth;
-  var treeWidth = graphWidth / (treeData.length);
-  var namesHeight = document.getElementById('nameRow').offsetHeight;
-  var rainHeight = document.getElementById('rainRow').offsetHeight;
+var graphWidth = window.innerWidth;
+var graphHeight = window.innerHeight;
 
-  var nameLabelRow = d3.select('#nameRow')
-    .append("svg")
-    .attr("width", graphWidth)
-    .attr("height", namesHeight);
+var imgURL = "http://worldartsme.com/images/truffula-tree-clipart-1.jpg";
 
-  var label = nameLabelRow.selectAll("text")
-    .data(treeData)
-    .enter()
-    .append("text")
-    .attr("width", treeWidth - barPadding)
-    .attr("text-anchor", "middle")
-    .attr("dominant-baseline", "middle")
-    .attr("transform", function(tree, i){
-      return "translate(" + [((treeWidth * i) + (barPadding / 2) + (treeWidth/2)), 20] + ")";
-    })
-    .text(function(d) { return d.UserName; });
+var treeGraph = d3.select('svg')
+  .attr("width", graphWidth)
+  .attr("height", graphHeight);
 
-    var rainRow = d3.select("#rainRow");
+var biggestTreeSize = Math.max.apply(null, treeData.map(function(tree){return tree.size}));
 
-    var rain = rainRow.selectAll("div")
-    .data(treeData)
-    .enter()
-    .append("div")
-    .style("width", (treeWidth) + "px")
-    .style("height", rainHeight + "px")
-    .style("display", "inline-block")
-    .attr("transform", function(d, i) {
-        return "translate(" + [(treeWidth * i), 0] + ")";
-    })
-    // .style("background-color", "blue");
-    .style("background-image", function(d){
-      if (d.isRaining) return "none"
-      else             return "url(frontend/rainy-7.svg)";
-    });
+// TODO normalise the incoming data instead of growing trees according to largest
+var scaleFactor = graphHeight / (biggestTreeSize * 1.2);
+var barPadding = 5;
+var treeWidth = graphWidth / (treeData.length);
 
-  var treeGraph = d3.select('#treesRow')
-    .append("svg")
-    .attr("width", graphWidth)
-    .attr("height", graphHeight);
+treeGraph.append("defs")
+     .append('pattern')
+     .attr('id', 'bg')
+     .attr('patternContentUnits', 'objectBoundingBox')
+     .attr('width', 1)
+     .attr('height', 1)
+     .append("image")
+     .attr("xlink:href", "tree_tester.jpg")
+     .attr('x', 0)
+     .attr('y', 0)
+     .attr('width', 1)
+     .attr('height', 1);
 
-  treeGraph.append("defs")
-    .append('pattern')
-    .attr('id', 'bg')
-    .attr('patternContentUnits', 'objectBoundingBox')
-    .attr('width', 1)
-    .attr('height', 1)
-    .append("image")
-    .attr("xlink:href", "frontend/tree_tester.jpg")
-    .attr('x', 0)
-    .attr('y', 0)
-    .attr('width', 1)
-    .attr('height', 1);
-
-  var tree = treeGraph.selectAll("g")
+var tree = treeGraph.selectAll("g")
     .data(treeData)
     .enter()
     .append("g")
     .attr("transform", function(tree, i) {
-       return "translate(" + [treeWidth * i + barPadding / 2, 0] + ")";
+        return "translate(" + [treeWidth * i + barPadding / 2, 0] + ")";
     })
-    .attr("height", (graphHeight*0.5))
 
-  tree.append("rect")
-    .attr("y", function(d) {
-      return (graphHeight - ((d.Score/100)*graphHeight));
-    })
-    .attr("height", function(d) {
-      return (d.Score+"%");
-    })
-    .attr("width", treeWidth - barPadding)
-    .attr("fill", "url(#bg)");
+tree.append("text")
+  .attr("y", 20)
+  .attr("x", treeWidth/2)
+  .attr("text-anchor", "middle")
+  .attr("dominant-baseline", "middle")
+  .attr("width", treeWidth - barPadding)
+  .text(function(d) { return d.name; });
 
-});
+tree.append("rect")
+  .attr("y", function(d) {
+    return (graphHeight - (d.size * scaleFactor));
+  })
+  .attr("height", function(tree) {
+    return (tree.size * scaleFactor);
+  })
+  .attr("width", treeWidth - barPadding)
+  .attr("fill", "url(#bg)");
